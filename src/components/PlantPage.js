b18fce3f1938a4e58dd8b059107434cb87a74324
Elizabeth Dowling <dowlinge94@gmail.com>
2022-04-14 12:27:48 -0400

    fetch tested

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+
+  useEffect(()=> {
+    (async () => {
+      let req = await fetch(`http://localhost:6001/plants`)
+      let res = await req.json()
+      console.log("DATABASE RESPONSE", res)
+    })()
+  }, []) 
+
   return (
     <main>
       <NewPlantForm />
