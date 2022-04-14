a58d8aacfc1dda87e8fc846beafad8324e64f4c7
Elizabeth Dowling <dowlinge94@gmail.com>
2022-04-14 12:33:19 -0400

    state set and passed to PlantList

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,15 +1,17 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+const [plantData, setPlantData] = useState([])
 
   useEffect(()=> {
     (async () => {
       let req = await fetch(`http://localhost:6001/plants`)
       let res = await req.json()
       console.log("DATABASE RESPONSE", res)
+      setPlantData(res)
     })()
   }, []) 
 
@@ -17,7 +19,7 @@ function PlantPage() {
     <main>
       <NewPlantForm />
       <Search />
-      <PlantList />
+      <PlantList plantData={plantData}/>
     </main>
   );
 }
